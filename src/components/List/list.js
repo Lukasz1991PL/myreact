7093b693d6cc606526cc5c59807ba443019c36e3
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-09-09 16:56:08 +0200

    add first components

---
+++ src/components/List/list.js
@@ -0,0 +1,22 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        Things to do <span>soon</span>
+      </header>
+      <p>“Interesting things I want to check out”</p>
+      <section>
+        <aricle>
+          <h1>BOOKS</h1>
+        </aricle>
+        <aricle>
+          <h1>MOVIES</h1>
+        </aricle>
+        <aricle>
+          <h1>GAMES</h1>
+        </aricle>
+      </section>
+    </div>
+  );
+};
+export default List;
