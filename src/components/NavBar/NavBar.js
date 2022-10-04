803dcdcc52ac861140ce21e2ed343faeb357e761
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-10-04 18:14:43 +0200

    add Links in Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import { Container } from '../Container/Container';
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+
 export const NavBar = () => {
   <nav className={styles.navbar}>
     <Container>
@@ -8,13 +10,13 @@ export const NavBar = () => {
       </a>
       <ul>
         <li>
-          <a href='/'>Home</a>
+          {/* <NavLink className={({isActive)} => ? styles.linkActive : undefined} to='/'>Home</Navlink> */}
         </li>
         <li>
-          <a href='/about'>About</a>
+          <Link href='/about'>About</Link>
         </li>
         <li>
-          <a href='/favourite'>Favourite</a>
+          <Link href='/favourite'>Favourite</Link>
         </li>
       </ul>
     </Container>
